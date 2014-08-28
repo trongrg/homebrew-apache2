ddce305ab05d4bf501224770f8b05d58fd777704
Trong Tran <trong.tran@sstechvn.com>
2014-08-28 23:32:49 +0700

    Upgraded to apache 2.4.9

--- apache24.rb
+++ apache24.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class Apache24 < Formula
   homepage 'https://httpd.apache.org/'
-  url 'http://mirror.cc.columbia.edu/pub/software/apache/httpd/httpd-2.4.7.tar.bz2'
-  sha1 '19ed9ee56462e44d61a093ea57e964cf0af05c0e'
+  url 'http://mirrors.digipower.vn/apache/httpd/httpd-2.4.9.tar.gz'
+  sha1 '50496e51605a3d852c183a7c667c25bcc7ee658d'
 
   skip_clean ['bin', 'sbin', 'logs']
 
@@ -17,7 +17,7 @@ class Apache24 < Formula
   # Apache 2.4 no longer bundles apr or apr-util so we have to fetch
   # it manually for each build
   def fetch_apr
-    ["apr-1.5.0", "apr-util-1.5.3"].each do |tb|
+    ["apr-1.5.1", "apr-util-1.5.3"].each do |tb|
       curl "-s", "-o", "#{tb}.tar.gz", "https://www.apache.org/dist/apr/#{tb}.tar.gz"
       system "tar -xzf #{tb}.tar.gz"
       dir = tb.rpartition('-')[0]
@@ -44,6 +44,7 @@ class Apache24 < Formula
     ]
     args << "--enable-lua" if build.with? 'lua'
 
+    p './configure ' + args.join(' ')
     system './configure', *args
     system "make"
     system "make install"
